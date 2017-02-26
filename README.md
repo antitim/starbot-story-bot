4f97468dcc55fbae714896808aed97c36f19fd48
antitim <antitim@yandex.ru>
2017-02-26 20:14:49 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# starbot-story-bot [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url]
+
+## Лицензия
+
+MIT © [antitim](http://vk.com/antitim)
+
+
+[npm-image]: https://badge.fury.io/js/starbot-story-bot.svg
+[npm-url]: https://npmjs.org/package/starbot-story-bot
+[travis-image]: https://travis-ci.org/antitim/starbot-story-bot.svg?branch=master
+[travis-url]: https://travis-ci.org/antitim/starbot-story-bot
---
+++ test/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+require('chai').should();
+const botScript = require('./test');
+const StoryBot = require('../lib');
+const Store = require('starbot-store-object');
+
+const storeInstance = new Store();
+const st = storeInstance.run('textBotName');
+
+const bot = StoryBot(botScript, st);
+
+describe('story-bot', () => {
+  it('greeting', async () => {
+    let answer = await bot('userId', 'Привет, Бот');
+    answer.text.should.equal('Привет дорогой читатель. Я бот. Как тебя зовут?');
+  });
+  it('myname', async () => {
+    let answer = await bot('userId', 'Максим');
+    answer.text.should.equal('Приятно познакомиться, Максим. Выбери вариант: 1 или 2?');
+  });
+  it('choice', async () => {
+    let answer = await bot('userId', '1');
+    answer.text.should.equal('Ты выбрал первый вариант. Отлично. Теперь я телепортирую тебя в случайное место');
+  });
+});
